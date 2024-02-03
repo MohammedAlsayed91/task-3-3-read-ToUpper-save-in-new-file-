04cd7814e51fa9eac8645da1471a8154086cb4ca
MohammedAlsayed91 <140766849+MohammedAlsayed91@users.noreply.github.com>
2024-02-03 18:40:07 +0300

    Add files via upload

---
+++ task 3-3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_3_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = @"C:\Users\User\OneDrive\Desktop\file\task3-3.txt";
+
+
+            //FileInfo file = new FileInfo(location);
+            //file.Create();
+            //Console.WriteLine("successfully created");
+
+
+            using (StreamWriter sw = File.AppendText(location))
+            {
+                var x = Console.ReadLine();
+                sw.WriteLine(x);
+
+                sw.Close();
+                Console.WriteLine("done");
+            }
+
+
+
+
+            if (File.Exists(location))
+            {
+                string[] lines;
+                using (StreamReader reader = new StreamReader(location))
+                {
+                    lines = File.ReadAllLines(location);
+                }
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                  
+                    
+                    lines[i] = lines[i].ToUpper();
+                }
+
+
+                string newfile = Path.ChangeExtension(location, "null")+"ToUpper.txt";
+                File.WriteAllLines(newfile, lines);
+
+                Console.WriteLine("File copied and changend to upper case successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Original file does not exist.");
+            }
+        }
+    }
+}
